4100c1c0c6d1ba5fa62e72c468518d4c5e15f1d2
burziszcze <buurzuum@gmail.com>
2018-10-10 18:51:38 +0200

    added redux, some routes and working on sass

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,59 @@
 import React, { Component } from 'react';
-// import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import jwt_decode from 'jwt-decode';
+import setAuthToken from './utils/setAuthtoken';
+import { setCurrentUser, logoutUser } from './actions/authActions';
+// import { clearCurrentProfile } from "./actions/profileActions";
+
+// redux
+import { Provider } from "react-redux";
+import store from "./store";
 
 // components
+import PrivateRoute from './components/common/PrivateRoute';
 import Header from './components/layout/Header';
 import Footer from './components/layout/Footer';
+import Register from "./components/auth/Register";
+import Login from "./components/auth/Login";
+
+// import styles
+import './styles/App.css';
+
+// check for Token
+if (localStorage.jwtToken) {
+  // Set auth token header auth
+  setAuthToken(localStorage.jwtToken);
+  // Decode token and get user info and expiration
+  const decoded = jwt_decode(localStorage.jwtToken);
+  // Set user and isAuthenticated
+  store.dispatch(setCurrentUser(decoded));
+  // Check for expired token
+  const currentTime = Date.now() / 1000;
+  if (decoded.exp < currentTime) {
+    // Logout user
+    store.dispatch(logoutUser());
+    // Clear current Profile
+    // store.dispatch(clearCurrentProfile());
+    // Redirect to login
+    window.location.href = '/login';
+  }
+}
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Header></Header>
-        <Footer></Footer>
-      </div>
+      <Provider store={store}>
+        <Router>
+          <div className="App">
+            <Header></Header>
+            <main role="main" className="container">
+              <Route exact path="/register" component={Register} />
+              <Route exact path="/Login" component={Login} />
+            </main>
+            <Footer></Footer>
+          </div>
+        </Router>
+      </Provider>
     );
   }
 }
--- client/src/components/layout/Footer.js
+++ client/src/components/layout/Footer.js
@@ -3,7 +3,11 @@ import React from 'react'
 const Footer = () => {
   return (
     <footer className="footer">
-      <h2>Copyright by Paul Vegan</h2>
+      <div className="container">
+        <span className="footer-header text-center">
+          <p>Copyright by Paul Vegan</p>
+        </span>
+      </div>
     </footer>
   )
 }
