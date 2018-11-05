a87e948ec0c45a7878f31726e2af4c28148eabaf
burziszcze <buurzuum@gmail.com>
2018-11-05 11:21:35 +0100

    fixing heroku dpeloy

--- server.js
+++ server.js
@@ -40,8 +40,8 @@ mongoose
 if (process.env.NODE_ENV === "production") {
   // Set static folder
   app.use(express.static("client/build"));
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  app.get("/*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "../client/build", "index.html"));
   });
 }
 
